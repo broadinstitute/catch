fd56366b5bb06282c641d028c7380a36e7058586
Hayden Metsky <hmetsky@gmail.com>
2015-02-17 14:30:08 -0500

    Add filtering approach unit tests

---
+++ hybseldesign/filter/tests/test_candidate_probes.py
@@ -0,0 +1,88 @@
+"""Tests for candidate_probes module.
+"""
+
+# Author: Hayden Metsky <hayden@mit.edu>
+
+import unittest
+
+from hybseldesign.utils import seq_io                                  
+from hybseldesign.datasets import ebola2014
+from hybseldesign.filter import candidate_probes
+
+
+"""Tests explicitly the generated candidate probes on contrived
+inputs."""
+class TestCandidateProbesOnContrivedInput(unittest.TestCase):
+
+  def test_no_n(self):
+    p = candidate_probes.make_candidate_probes_from_sequence(
+              'ATCGTCGCGGATCG', probe_length=6, probe_stride=3,
+              min_n_string_length=2)
+    p = [x.seq.tostring() for x in p]
+    self.assertItemsEqual(p,
+        ['ATCGTC', 'GTCGCG', 'GCGGAT', 'GGATCG'])
+
+  def test_one_n(self):
+    p = candidate_probes.make_candidate_probes_from_sequence(
+              'ATCGNCGCGGATCG', probe_length=6, probe_stride=3,
+              min_n_string_length=2)
+    p = [x.seq.tostring() for x in p]
+    self.assertItemsEqual(p,                                          
+        ['ATCGNC', 'GNCGCG', 'GCGGAT', 'GGATCG'])
+
+  def test_two_n(self):
+    p = candidate_probes.make_candidate_probes_from_sequence(         
+              'ATNGNCGCGGATCG', probe_length=6, probe_stride=3,       
+              min_n_string_length=2)                                  
+    p = [x.seq.tostring() for x in p]                                 
+    self.assertItemsEqual(p,                                          
+        ['ATNGNC', 'GNCGCG', 'GCGGAT', 'GGATCG']) 
+
+  def test_n_string1(self):
+    p = candidate_probes.make_candidate_probes_from_sequence(
+              'ATCGNCGNNTCG', probe_length=6, probe_stride=3,
+              min_n_string_length=2)
+    p = [x.seq.tostring() for x in p]
+    self.assertItemsEqual(p, ['ATCGNC', 'TCGNCG', 'TCGNCG'])
+
+  def test_n_string2(self):
+    p = candidate_probes.make_candidate_probes_from_sequence(
+              'ATCGNCGNNTCGATAT', probe_length=6, probe_stride=3,
+              min_n_string_length=2)
+    p = [x.seq.tostring() for x in p]
+    self.assertItemsEqual(p,
+        ['ATCGNC', 'TCGNCG', 'TCGATA', 'TCGNCG', 'TCGATA',
+        'TCGATA', 'CGATAT'])
+    
+  def test_multiple_seqs(self):
+    p = candidate_probes.make_candidate_probes_from_sequences(
+              ['ATCGNCGNNTCG', 'ATCGNCGNNTCGATAT'],
+              probe_length=6, probe_stride=3, min_n_string_length=2)
+    p = [x.seq.tostring() for x in p]
+    self.assertItemsEqual(p, ['ATCGNC', 'TCGNCG', 'TCGNCG'] + \
+        ['ATCGNC', 'TCGNCG', 'TCGATA', 'TCGNCG', 'TCGATA',            
+        'TCGATA', 'CGATAT'])
+
+
+"""Tests the candidate probes from the Ebola 2014 dataset.
+"""
+class TestCandidateProbesOnEbola2014(unittest.TestCase):
+
+  def setUp(self):
+    seqs = seq_io.read_fasta(ebola2014.fasta_path()).values()
+    self.probes = candidate_probes.\
+        make_candidate_probes_from_sequences(seqs,
+          probe_length=100, probe_stride=50,
+          min_n_string_length=2)
+
+  """Test that all probes are 100 bp.
+  """
+  def test_probe_length(self):
+    for probe in self.probes:
+      self.assertEqual(len(probe.seq), 100)
+
+  """Test that no probes have a string of two or more 'N's.
+  """
+  def test_n_string(self):
+    for probe in self.probes:
+      self.assertNotIn('NN', probe.seq.tostring())
---
+++ hybseldesign/filter/tests/test_duplicate_filter.py
@@ -0,0 +1,33 @@
+"""Tests for duplicate_filter module.
+"""
+
+# Author: Hayden Metsky <hayden@mit.edu>
+
+import unittest
+
+from hybseldesign import probe
+from hybseldesign.filter import duplicate_filter
+
+
+"""Tests the duplicate filter output on contrived input.
+"""
+class TestDuplicateFilter(unittest.TestCase):
+
+  def test_basic(self):
+    input = ['ATCGTCGCGG',
+             'ATCGTAGCGG',
+             'ATCGTCACGG',
+             'ATCGTAGCGG',
+             'ATTGTCGCGG',
+             'ATCGTCGCGG']
+    desired_output = ['ATCGTCGCGG',
+                      'ATCGTAGCGG',
+                      'ATCGTCACGG',
+                      'ATTGTCGCGG']
+    input_probes = [probe.Probe.from_str(s) for s in input]
+    desired_output_probes = [probe.Probe.from_str(s) for \
+                              s in desired_output]
+    f = duplicate_filter.DuplicateFilter()
+    f.filter(input_probes)
+    self.assertItemsEqual(f.input_probes, input_probes)
+    self.assertItemsEqual(f.output_probes, desired_output_probes)
