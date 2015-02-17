476c87bd820bae4390cc3485750fdf9549d35e3c
Hayden Metsky <hmetsky@gmail.com>
2015-02-17 14:27:28 -0500

    Add probe unit tests

---
+++ hybseldesign/tests/test_probe.py
@@ -0,0 +1,47 @@
+"""Tests for probe module.
+"""
+
+# Author: Hayden Metsky <hayden@mit.edu>
+
+import unittest
+import numpy as np
+
+from hybseldesign import probe
+
+
+"""Tests methods in the Probe class.
+"""
+class TestProbe(unittest.TestCase):
+
+  def setUp(self):
+    self.a = probe.Probe.from_str('ATCGTCGCGGATCG')
+    self.b = probe.Probe.from_str('ATCCTCGCGTATNG')
+    self.c = probe.Probe.from_str('ATCGTCGCGGATC')
+    self.d = probe.Probe.from_str('GATCGTCGCGGATC')
+    self.e = probe.Probe.from_str('GGATTGTCGGGGAT')
+    self.f = probe.Probe.from_str('GTCGCGGAACGGGG')
+
+  """Test that probe parses the string correctly.
+  """
+  def test_parse_str(self):
+    np.testing.assert_array_equal(self.a.seq,
+        np.array(['A','T','C','G','T','C','G','C','G',
+                  'G','A','T','C','G']))
+
+  """Test mismatches method.
+  """
+  def test_mismatches(self):
+    self.assertEqual(self.a.mismatches(self.a), 0)
+    self.assertEqual(self.a.mismatches(self.b), 3)
+    self.assertEqual(self.b.mismatches(self.a), 3)
+
+  """Test mismatches_at_offset method.
+  """
+  def test_mismatches_at_offset(self):
+    self.assertEqual(self.a.mismatches_at_offset(self.d, 1), 0)
+    self.assertEqual(self.a.mismatches_at_offset(self.e, 2), 2)
+    self.assertEqual(self.a.mismatches_at_offset(self.f, 3), 1)
+    self.assertRaises(ValueError, self.a.mismatches_at_offset,
+        self.c, 1)
+    self.assertRaises(ValueError, self.a.mismatches_at_offset,
+        self.b, 15)
