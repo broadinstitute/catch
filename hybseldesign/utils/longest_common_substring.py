3e5924f51f024bc3a12223dad720c609764bc939
Hayden Metsky <hmetsky@gmail.com>
2015-02-18 18:13:19 -0500

    Implement longest common substring with k mismatches

---
+++ hybseldesign/utils/longest_common_substring.py
@@ -0,0 +1,50 @@
+"""Functions for computing the longest common substring between two
+sequences, a problem that is potentially relevant to probe design.
+"""
+
+# Author: Hayden Metsky <hayden@mit.edu>
+
+from collections import deque
+
+
+"""Computes the longest common substring with k mismatches.
+
+The two sequences, a and b, can be numpy arrays or Python strings.
+The algorithm runs in O(|a|*|b|) time and requires O(k) space.
+It returns, in order, the length of the longest common substring,
+the starting position of the substring in a, and the starting
+position of the substring in b.
+
+The implementation is of an algorithm published in the 2014 paper
+"Longest common substrings with k mismatches"
+  by Flouri, Giaquinta, Kobert, and Ukkonen
+  (http://arxiv.org/pdf/1409.1694v1.pdf)
+It is an implementation of the pseudocode in Figure 1 of the paper.
+Note that the pseudocode is meant for k>0; the code below
+includes a modification to support k=0.
+"""
+def k_lcf(a, b, k):
+  n = len(a)
+  m = len(b)
+  ell, r_a, r_b = 0, 0, 0
+  for d in xrange(-m+1, n):
+    i = max(-d, 0) + d
+    j = max(-d, 0)
+    Q = deque([])
+    s, l = 0, 0
+    while l <= min(n-i, m-j) - 1:
+      if a[i+l] != b[j+l]:
+        if k == 0:
+          s = l+1
+        else:
+          if len(Q) == k:
+            s = min(Q) + 1
+            Q.popleft()
+          Q.append(l)
+      l = l+1
+      if l-s > ell:
+        ell = l-s
+        r_a = i+s
+        r_b = j+s
+  return ell, r_a, r_b
+
---
+++ hybseldesign/utils/tests/test_longest_common_substring.py
@@ -0,0 +1,56 @@
+"""Tests for longest_common_substring module.
+"""
+
+# Author: Hayden Metsky <hayden@mit.edu>
+
+import unittest
+
+from hybseldesign.utils import longest_common_substring as lcf
+
+
+"""Tests the longest common substring with k mismatches
+implementation.
+"""
+class TestLongestCommonSubstringWithKMismatches(unittest.TestCase):
+
+  def test_equal(self):
+    a = 'ABCDEFGHIJ'
+    b = 'ABCDEFGHIJ'
+    for k in xrange(0, 15):
+      self.assertEqual(lcf.k_lcf(a,b,k), (10,0,0))
+
+  def test_almost_equal1(self):
+    a = 'ABCDEFGHIJ'
+    b = 'ABCDEFGKIJ'
+    self.assertEqual(lcf.k_lcf(a,b,0), (7,0,0))
+    for k in xrange(1, 15):
+      self.assertEqual(lcf.k_lcf(a,b,k), (10,0,0))
+
+  def test_almost_equal2(self):
+    a = 'ABCABCDEFGHIJKLM'
+    b = 'DEFGABCDEFGHIJKLN'
+    self.assertEqual(lcf.k_lcf(a,b,0), (12,3,4))
+    self.assertEqual(lcf.k_lcf(a,b,1), (13,2,3))
+    self.assertEqual(lcf.k_lcf(a,b,2), (14,1,2))
+
+  def test_complex1(self):
+    a = 'ABCDEFLONGESTCOMMONSUBSTRINGQRSTU'
+    b = 'QRSLONGESTCOMMONTUBVTRINGVYX'
+    self.assertEqual(lcf.k_lcf(a,b,0), (13,6,3))
+    self.assertEqual(lcf.k_lcf(a,b,1), (16,6,3))
+    self.assertEqual(lcf.k_lcf(a,b,2), (22,6,3))
+    self.assertEqual(lcf.k_lcf(a,b,3), (23,5,2))
+    self.assertEqual(lcf.k_lcf(a,b,4), (24,4,1))
+    self.assertEqual(lcf.k_lcf(a,b,5), (25,3,0))
+    self.assertEqual(lcf.k_lcf(a,b,6), (26,3,0))
+    self.assertEqual(lcf.k_lcf(a,b,7), (27,3,0))
+
+  def test_complex2(self):
+    a = 'AGTCGCTGCCTCGTGCACATTG'
+    b = 'GTATAATGTCGCAGCGTCGGCC'
+    self.assertEqual(lcf.k_lcf(a,b,0), (5,1,7))
+    self.assertEqual(lcf.k_lcf(a,b,1), (8,1,7))
+    self.assertEqual(lcf.k_lcf(a,b,2), (12,1,7))
+    self.assertEqual(lcf.k_lcf(a,b,3), (13,0,6))
+    self.assertEqual(lcf.k_lcf(a,b,4), (15,1,7))
+
