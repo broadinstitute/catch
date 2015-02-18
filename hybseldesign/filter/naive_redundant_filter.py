779e6c454fd48f7b7d5911112d152dc8bd49fe22
Hayden Metsky <hmetsky@gmail.com>
2015-02-18 18:13:40 -0500

    Comment the naive redundant filter

--- hybseldesign/filter/naive_redundant_filter.py
+++ hybseldesign/filter/naive_redundant_filter.py
@@ -1,9 +1,21 @@
-"""Filters a set of candidate probes in a naive way: by iterating
-through the set and, for each possible probe P, removing all others
-that satisfy a specified similarity criterion with P.
+"""Reduces a set of candidate probes in a naive way: by iterating
+through the set and, for each candidate probe P, removing all others
+that satisfy a specified similarity criterion with P (i.e., are
+deemed redundant to P).
+
+Note that this is just a heuristic with no guarantee to minimize the
+number of probes. The output is dependent on the order in which we
+iterate through the probes and it is not obvious whether this even
+yields a reasonable approximation in an average case. This was
+implemented primarily to replicate previous software and results on
+designing probes for hybrid selection.
 """
 
 # Author: Hayden Metsky <hayden@mit.edu>
 
+from hybseldesign.filter.base_filter import BaseFilter
 
-#TODO: implement
+
+class NaiveRedundantFilter(BaseFilter):
+
+  def _filter(self, input):
