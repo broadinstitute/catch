0dde6a5209bb63dca192bfd627a1cc1bb9e904e9
Hayden Metsky <hmetsky@gmail.com>
2015-02-19 12:34:31 -0500

    Undo update to make probe script

--- bin/make_ebola2014_probes.py
+++ bin/make_ebola2014_probes.py
@@ -4,14 +4,11 @@
 
 # Author: Hayden Metsky <hayden@mit.edu>
 
-import sys
-
 from hybseldesign.datasets import ebola2014
 from hybseldesign.utils import seq_io
 from hybseldesign.filter import probe_designer
 from hybseldesign.filter import reverse_complement_filter
 from hybseldesign.filter import duplicate_filter
-from hybseldesign.filter import naive_redundant_filter
 
 
 # For now, simply:
@@ -20,16 +17,7 @@ from hybseldesign.filter import naive_redundant_filter
 seqs = seq_io.read_fasta(ebola2014.fasta_path()).values()
 rc = reverse_complement_filter.ReverseComplementFilter()
 df = duplicate_filter.DuplicateFilter()
-nf = naive_redundant_filter.NaiveRedundantFilter(
-      naive_redundant_filter.redundant_shift_and_mismatch_count())
-filters = []
-if "rc" in sys.argv[1]:
-  filters += [rc]
-if "df" in sys.argv[1]:
-  filters += [df]
-if "nf" in sys.argv[1]:
-  filters += [nf]
-pb = probe_designer.ProbeDesigner(seqs, filters)
+pb = probe_designer.ProbeDesigner(seqs, [rc, df])
 pb.design()
 
 print "Number of candidate probes:", len(pb.candidate_probes)
