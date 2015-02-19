1f684e322e205cbf93a73115e7056d627ffc3870
Hayden Metsky <hmetsky@gmail.com>
2015-02-18 21:23:22 -0500

    Add functions that determine whether two probes are redundant

--- hybseldesign/filter/naive_redundant_filter.py
+++ hybseldesign/filter/naive_redundant_filter.py
@@ -19,3 +19,39 @@ from hybseldesign.filter.base_filter import BaseFilter
 class NaiveRedundantFilter(BaseFilter):
 
   def _filter(self, input):
+    pass
+
+
+"""Returns a function that determines whether two probes are
+redundant based on the minimum number of mismatches as one probe
+is shifted relative to the other.
+
+One probe is shifted between -shift and +shift relative to the
+other. If the smallest number of mismatches encountered is
+is <= mismatch_thres, then are_redundant outputs True; otherwise
+it outputs False.
+"""
+def are_redundant_based_on_shift_and_mismatch_count(shift=0,
+    mismatch_thres=0):
+  def are_redundant(probe_a, probe_b):
+    mismatches = probe_a.min_mismatches_within_shift(probe_b, shift)
+    return mismatches <= mismatch_thres
+  return are_redundant
+
+
+"""Returns a function that determines whether two probes are
+redundant based on the length of the longest common substring
+with less than or equal to 'mismatches' mismatches.
+
+If the length of the longest common substring with at most
+'mismatches' mismatches is >= lcf_thres, then are_redundant outputs
+True; otherwise it outputs False.
+"""
+def are_redundant_based_on_longest_common_substring(mismatches=0,
+    lcf_thres=100):
+  def are_redundant(probe_a, probe_b):
+    lcf_length = probe_a.longest_common_substring_length(probe_b,
+                    mismatches)
+    return lcf_length >= lcf_thres
+  return are_redundant
+  
