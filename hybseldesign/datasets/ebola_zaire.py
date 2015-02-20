bc37f7e495fe43bdacf1c61d646c0bcee2c7c6b7
Hayden Metsky <hmetsky@gmail.com>
2015-02-19 20:24:49 -0500

    Add Ebola Zaire dataset

---
+++ hybseldesign/datasets/ebola_zaire.py
@@ -0,0 +1,13 @@
+"""A dataset with 140 Ebola samples taken from Zaire.
+"""
+
+# Author: Hayden Metsky <hayden@mit.edu>
+
+from os.path import join
+from os.path import dirname
+
+FASTA_RELATIVE_PATH = "data/ebola_zaire.fasta"
+
+
+def fasta_path():
+  return join(dirname(__file__), FASTA_RELATIVE_PATH)
