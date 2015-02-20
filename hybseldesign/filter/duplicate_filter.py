36d2f3e4203f3f7e03f41f082c9c9ceb997fe2e2
Hayden Metsky <hmetsky@gmail.com>
2015-02-20 18:16:52 -0500

    Preserve input order in duplicate_filter

--- hybseldesign/filter/duplicate_filter.py
+++ hybseldesign/filter/duplicate_filter.py
@@ -1,12 +1,13 @@
 """Filters probes by returning a list of non-duplicate probes from
 the given input probes.
 
-This only removes probes that are identical and makes no
-guarantees about the order of what is returned (i.e., it is
-not stable with respect to the input order).
+This only removes probes that are identical to another. It preserves
+the order of the input.
 """
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+from collections import OrderedDict
 
 from hybseldesign.filter.base_filter import BaseFilter
 
@@ -14,4 +15,8 @@ from hybseldesign.filter.base_filter import BaseFilter
 class DuplicateFilter(BaseFilter):
 
   def _filter(self, input):
-    return list(set(input))
+    # `return list(set(input))` would be a short way to produce
+    # non-duplicate probes, but would not preserve the input
+    # order. Instead, preserve the order with an OrderedDict.
+    return list(OrderedDict.fromkeys(input))
+
