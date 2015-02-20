f2990a3095446f9568c65b9bbbcdd85dee87e5a1
Hayden Metsky <hmetsky@gmail.com>
2015-02-19 19:02:09 -0500

    Add bug to candidate probe designer to deliberately replicate Matlab code

--- hybseldesign/filter/candidate_probes.py
+++ hybseldesign/filter/candidate_probes.py
@@ -16,16 +16,26 @@ from hybseldesign.datasets import ebola2014
 """Generates and returns a list of candidate probes from the sequence
 seq.
 
-The probes are probe_length bp long and separated by probeStride bp.
+The probes are probe_length bp long and separated by probe_stride bp.
 Possible probes that would contains strings of min_n_string_length or
 more N's are discarded and, instead, new probes flanking the string
 are added.
 
-It is possible (likely, in fact, when there are strings of
-N's) that duplicate probes are returned.
+When the sequence length is not divisible by probe_stride there are
+remaining bases at the end not covered. An extra probe is added for
+these iff add_probe_for_end_bases is True. (This argument was added
+to replicate past software and results, in which its value would be
+False.)
+
+insert_bug adds a bug to the code in order to replicate past software
+(the first version of probe design, as Matlab code) and its reuslts.
+
+It is possible (especially when there are strings of N's) that
+duplicate probes are returned.
 """
 def make_candidate_probes_from_sequence(seq, probe_length=100,
-    probe_stride=50, min_n_string_length=2):
+    probe_stride=50, min_n_string_length=2,
+    add_probe_for_end_bases=True, insert_bug=False):
   if probe_length > len(seq):
     raise ValueError("Invalid probe_length " + str(probe_length))
 
@@ -33,52 +43,29 @@ def make_candidate_probes_from_sequence(seq, probe_length=100,
     seq = seq.tostring()
   n_string_query = re.compile('(N{'+str(min_n_string_length)+',})')
 
-  # Make one or two probes based on the subsequence seq[start:end].
+  # Make a probe based on the subsequence seq[start:end].
   # Namely, if that subsequence contains no string of N's, then it
-  # is itself a probe to be added. But if it does contain a string
-  # of N's, then add two flanking probes as needed.
-  def add_probe_from_subsequence(start, end):
+  # is a probe to be added and the probe is returned in a single-
+  # valued list. Otherwise, an empty list is returned.
+  def add_probe_from_subsequence(start, end, is_bug_location=False):
     subseq = seq[start:end]
     probes = []
 
-    # Search for strings of min_n_string_length or more N's in subseq
-    # (Add start onto match.start() and match.end() because the
-    #  search is within subseq, but the coordinates should be
-    #  relative to seq.)
-    found_n_string = False
-    n_string_starts, n_string_ends = [], []
-    for match in n_string_query.finditer(subseq):
-      found_n_string = True
-      n_string_starts += [start + match.start()]
-      n_string_ends += [start + match.end()]
-
-    if found_n_string:
-      # There is indeed at least one string of N's, so add flanking
-      # probes. Specifically, add a probe to the left of the
-      # leftmost string and a probe to the right of the rightmost
-      # string.
-      # (If we were to add a right probe to the leftmost string, a
-      #  a left probe to the rightmost string, or flanking probes on
-      #  middle strings, these would overlap strings of N's.)
-      leftmost_start = min(n_string_starts)
-      rightmost_end = max(n_string_ends)
-      if leftmost_start - probe_length >= 0:
-        left_subseq = seq[(leftmost_start-probe_length):leftmost_start]
-        if not n_string_query.search(left_subseq):
-          # Only add the flanking probe if it doesn't contain a
-          # string of N's (i.e., don't recursively chase flanking
-          # probes)
-          probes += [left_subseq]
-      if rightmost_end + probe_length <= len(seq):
-        right_subseq = seq[rightmost_end:(rightmost_end+probe_length)]
-        if not n_string_query.search(right_subseq):
-          # Only add the flanking probe if it doesn't contain a
-          # string of N's (i.e., don't recursively chase flanking
-          # probes)
-          probes += [right_subseq]
+    if insert_bug and is_bug_location:
+      if 'N' in subseq:
+        # A bug in the original version of the probe design (Matlab code)
+        # considered a probe with just a single 'N' (even if
+        # min_n_string_length > 1) to be invalid and otherwise to be
+        # valid
+        pass
+      else:
+        probes += [subseq]
     else:
-      # There's no string of N's, so this subsequence is a valid probe
-      probes += [subseq]
+      # Search for strings of min_n_string_length or more N's in subseq
+      # and only add a probe if there is not such a string
+      if not n_string_query.search(subseq):
+        # There's no string of N's, so this subsequence is a valid probe
+        probes += [subseq]
 
     return probes
 
@@ -87,11 +74,29 @@ def make_candidate_probes_from_sequence(seq, probe_length=100,
   for start in np.arange(0, len(seq), probe_stride):
     if start + probe_length > len(seq):
       break
-    probes += add_probe_from_subsequence(start, start+probe_length)
-  if len(seq) % probe_stride != 0:
+    # A bug in the original version of the probe design (Matlab code)
+    # was present in designing a so-called 'B adapter'
+    is_bug_location = start % probe_length == probe_stride
+    probes += add_probe_from_subsequence(start, start+probe_length,
+                is_bug_location=is_bug_location)
+  if len(seq) % probe_stride != 0 and add_probe_for_end_bases:
     # There are bases on the right that were never covered, so add
     # another probe for this
     probes += add_probe_from_subsequence(len(seq)-probe_length, len(seq))
+
+  # Add probes flanking each string of N's. Specifically, add a probe
+  # to the left of a string and to the right. The called function
+  # must check that the flanking probe does not contain a string of
+  # N's before adding. (Don't recursively chase flanking probes.)
+  for match in n_string_query.finditer(seq):
+    if match.start() - probe_length >= 0:
+      # Add the left flanking probe for match
+      probes += add_probe_from_subsequence(match.start()-probe_length,
+                  match.start())
+    if match.end() + probe_length <= len(seq):
+      # Add the right flanking probe for match
+      probes += add_probe_from_subsequence(match.end(),
+                  match.end()+probe_length)
 
   # Convert the probes from a Python list of Python strings to a
   # list of probe.Probe
@@ -108,7 +113,8 @@ It is possible (perhaps even likely depending on where
 the sequences come from) that duplicate probes are returned.
 """
 def make_candidate_probes_from_sequences(seqs, probe_length=100,        
-    probe_stride=50, min_n_string_length=2):
+    probe_stride=50, min_n_string_length=2,
+    add_probe_for_end_bases=True, insert_bug=False):
   if type(seqs) != list:
     raise ValueError("seqs must be a list of sequences")
   if len(seqs) == 0:
@@ -121,6 +127,8 @@ def make_candidate_probes_from_sequences(seqs, probe_length=100,
   for seq in seqs:
     probes += make_candidate_probes_from_sequence(seq,
                 probe_length=probe_length, probe_stride=probe_stride,
-                min_n_string_length=min_n_string_length)
+                min_n_string_length=min_n_string_length,
+                add_probe_for_end_bases=add_probe_for_end_bases,
+                insert_bug=insert_bug)
   return probes
 
--- hybseldesign/filter/tests/test_candidate_probes.py
+++ hybseldesign/filter/tests/test_candidate_probes.py
@@ -17,7 +17,7 @@ class TestCandidateProbesOnContrivedInput(unittest.TestCase):
   def test_no_n(self):
     p = candidate_probes.make_candidate_probes_from_sequence(
               'ATCGTCGCGGATCG', probe_length=6, probe_stride=3,
-              min_n_string_length=2)
+              min_n_string_length=2, add_probe_for_end_bases=True)
     p = [x.seq.tostring() for x in p]
     self.assertItemsEqual(p,
         ['ATCGTC', 'GTCGCG', 'GCGGAT', 'GGATCG'])
@@ -25,7 +25,7 @@ class TestCandidateProbesOnContrivedInput(unittest.TestCase):
   def test_one_n(self):
     p = candidate_probes.make_candidate_probes_from_sequence(
               'ATCGNCGCGGATCG', probe_length=6, probe_stride=3,
-              min_n_string_length=2)
+              min_n_string_length=2, add_probe_for_end_bases=True)
     p = [x.seq.tostring() for x in p]
     self.assertItemsEqual(p,                                          
         ['ATCGNC', 'GNCGCG', 'GCGGAT', 'GGATCG'])
@@ -33,7 +33,7 @@ class TestCandidateProbesOnContrivedInput(unittest.TestCase):
   def test_two_n(self):
     p = candidate_probes.make_candidate_probes_from_sequence(         
               'ATNGNCGCGGATCG', probe_length=6, probe_stride=3,       
-              min_n_string_length=2)                                  
+              min_n_string_length=2, add_probe_for_end_bases=True)                                  
     p = [x.seq.tostring() for x in p]                                 
     self.assertItemsEqual(p,                                          
         ['ATNGNC', 'GNCGCG', 'GCGGAT', 'GGATCG']) 
@@ -41,27 +41,26 @@ class TestCandidateProbesOnContrivedInput(unittest.TestCase):
   def test_n_string1(self):
     p = candidate_probes.make_candidate_probes_from_sequence(
               'ATCGNCGNNTCG', probe_length=6, probe_stride=3,
-              min_n_string_length=2)
+              min_n_string_length=2, add_probe_for_end_bases=True)
     p = [x.seq.tostring() for x in p]
-    self.assertItemsEqual(p, ['ATCGNC', 'TCGNCG', 'TCGNCG'])
+    self.assertItemsEqual(p, ['ATCGNC', 'TCGNCG'])
 
   def test_n_string2(self):
     p = candidate_probes.make_candidate_probes_from_sequence(
               'ATCGNCGNNTCGATAT', probe_length=6, probe_stride=3,
-              min_n_string_length=2)
+              min_n_string_length=2, add_probe_for_end_bases=True)
     p = [x.seq.tostring() for x in p]
     self.assertItemsEqual(p,
-        ['ATCGNC', 'TCGNCG', 'TCGATA', 'TCGNCG', 'TCGATA',
-        'TCGATA', 'CGATAT'])
+        ['ATCGNC', 'TCGNCG', 'TCGATA', 'TCGATA', 'CGATAT'])
     
   def test_multiple_seqs(self):
     p = candidate_probes.make_candidate_probes_from_sequences(
               ['ATCGNCGNNTCG', 'ATCGNCGNNTCGATAT'],
-              probe_length=6, probe_stride=3, min_n_string_length=2)
+              probe_length=6, probe_stride=3, min_n_string_length=2,
+              add_probe_for_end_bases=True)
     p = [x.seq.tostring() for x in p]
-    self.assertItemsEqual(p, ['ATCGNC', 'TCGNCG', 'TCGNCG'] + \
-        ['ATCGNC', 'TCGNCG', 'TCGATA', 'TCGNCG', 'TCGATA',            
-        'TCGATA', 'CGATAT'])
+    self.assertItemsEqual(p, ['ATCGNC', 'TCGNCG'] + \
+        ['ATCGNC', 'TCGNCG', 'TCGATA', 'TCGATA', 'CGATAT'])
 
 
 """Tests the candidate probes from the Ebola 2014 dataset.
@@ -73,7 +72,7 @@ class TestCandidateProbesOnEbola2014(unittest.TestCase):
     self.probes = candidate_probes.\
         make_candidate_probes_from_sequences(seqs,
           probe_length=100, probe_stride=50,
-          min_n_string_length=2)
+          min_n_string_length=2, add_probe_for_end_bases=True)
 
   """Test that all probes are 100 bp.
   """
