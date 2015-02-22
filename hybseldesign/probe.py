ddabf0c54032ff3f247fa995358deb2c06081bf9
Hayden Metsky <hmetsky@gmail.com>
2015-02-22 09:30:41 -0500

    Implement heuristic to prune comparisons of probes that are likely not redundant

--- hybseldesign/probe.py
+++ hybseldesign/probe.py
@@ -4,6 +4,7 @@ with them.
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
+from collections import defaultdict
 import numpy as np
 
 from hybseldesign.utils import longest_common_substring
@@ -14,8 +15,12 @@ from hybseldesign.utils import longest_common_substring
 class Probe:
   def __init__(self, seq):
     self.seq = seq
-    self.hash = hash(seq.tostring())
+    self.seq_str = seq.tostring()
     self.is_flanking_n_string = False
+
+    self.kmer_hashes = defaultdict(set)
+    self.kmer_hashes_rand_choices = defaultdict(
+                                       lambda : defaultdict(list))
 
   """Counts the number of mismatches between self and other.
 
@@ -70,8 +75,104 @@ class Probe:
                 dtype='S1')
     return Probe(rc_seq)
 
+  """Adds the hash of each k-mer of this probe to self.kmer_hashes.
+
+  This could add the k-mer itself, rather than the hash, to avoid
+  false positives (due to collisions) on lookup; however, storing
+  the hash rather than a k-mer gives space savings and avoids having
+  to compute the hash of a k-mer repeatedly on lookup.
+
+  Use hash(kmer) & 0xffffffff so that the stored hash is 32-bit
+  rather than 64-bit; 32-bit suffices here and halves the space
+  requirements.
+  """
+  def construct_kmer_hashes(self, k):
+    for i in xrange(len(self.seq)-k+1):
+      kmer = self.seq_str[i:(i+k)]
+      self.kmer_hashes[k].add(hash(kmer) & 0xffffffff)
+
+  """A heuristic that outputs whether it is likely that self and
+  other share at least one k-mer. Note that, depending on the
+  sequences being compared and the parameter values, false negatives
+  are a very real possibility.
+
+  When memoize_kmer_hashes is True, false positives are possible
+  although extremely unlikely; when it is False, there are no false
+  positives. In either case, false negatives are possible (indeed
+  even likely for two sequences with very few k-mers in common) and
+  their probability of occurring depends on the number of k-mers in
+  common and on num_kmers_to_test.
+
+  This heuristic is intended primarily for determining whether it
+  is possible that two sequences are 'redundant'. If two sequences
+  are 'redundant', they ought to (by definition) share many k-mers
+  and therefore this heuristic should have a small probability
+  of giving outputting False (a false negative). If two sequences
+  are not 'redundant', they should have few or no k-mers in common
+  and therefore this heuristic should have a small probability of
+  outputting True (a false positive). [In this paragraph, the notion
+  of a false positive/negative relates to whether the sequences are
+  'redundant', whereas in the earlier paragraph it relates to whether
+  they share at least one k-mer.]
+
+  In the case where two sequences are 'redundant', assume they have
+  N k-mers in common. The probability of this outputting False
+  (falsely indicating that the two are likely not redundant) is the
+  probability that this heuristic does not select any of those N
+  k-mers. That is:
+    ( 1 - N/(len(seq)-k+1) )^{num_kmers_to_test}
+
+  In the case where two sequences are not 'redundant', assume they
+  are each produced at random with uniformity and independence across
+  possible k-mers. The probability of this outputting True (falsely
+  indicating that the two might be redundant) is the probability that
+  this heuristic finds at least one k-mer in common. That is:
+    1 - ( (1-(1/4)^k )^{len(seq)-k+1} )^{num_kmers_to_test}
+  """
+  def shares_some_kmers(self, other, k=10, num_kmers_to_test=5,
+      memoize_kmer_hashes=True):
+    if memoize_kmer_hashes:
+      # Construct the k-mer hashes for self and other if they have
+      # not yet been constructed for the given k
+      if len(self.kmer_hashes[k]) == 0:
+        self.construct_kmer_hashes(k)
+      if len(other.kmer_hashes[k]) == 0:
+        other.construct_kmer_hashes(k)
+
+      if len(self.kmer_hashes_rand_choices[k][num_kmers_to_test]) == 0:
+        rand_kmer_hashes = np.random.choice(list(self.kmer_hashes[k]),
+            size=num_kmers_to_test, replace=True)
+        # Memoize the random choices too because the calls to
+        # list(self.kmer_hashes[k]) and to np.random.choice are
+        # slow
+        self.kmer_hashes_rand_choices[k][num_kmers_to_test] = \
+            rand_kmer_hashes
+      else:
+        rand_kmer_hashes = \
+            self.kmer_hashes_rand_choices[k][num_kmers_to_test]
+
+      other_kmer_hashes = other.kmer_hashes[k]
+      for rand_kmer_hash in rand_kmer_hashes:
+        if rand_kmer_hash in other_kmer_hashes:
+          # This may be a collision and it may be that in fact
+          # self and other do not share this k-mer, but we allow
+          # false positives
+          return True
+      return False
+    else:
+      rand_kmer_positions = np.random.random_integers(
+          0, len(self.seq)-k, num_kmers_to_test)
+      for n in xrange(num_kmers_to_test):
+        # Read a random k-mer from self and explicitly test for
+        # its presence in other
+        rand_kmer_pos = rand_kmer_positions[n]
+        rand_kmer = self.seq_str[rand_kmer_pos:(rand_kmer_pos+k)]
+        if rand_kmer in other.seq_str:
+          return True
+      return False
+
   def __hash__(self):
-    return self.hash
+    return hash(self.seq_str)
 
   def __eq__(self, other):
     return isinstance(other, Probe) and \
@@ -87,7 +188,7 @@ class Probe:
       return cmp(self.seq[c[0]], other.seq[c[0]])
 
   def __str__(self):
-    return self.seq.tostring()
+    return self.seq_str
 
   def __repr__(self):
     return str(self.seq)
--- hybseldesign/tests/test_probe.py
+++ hybseldesign/tests/test_probe.py
@@ -59,3 +59,51 @@ class TestProbe(unittest.TestCase):
     self.assertEqual(self.a.min_mismatches_within_shift(self.b, 2), 3)
     self.assertEqual(self.b.min_mismatches_within_shift(self.a, 2), 3)
 
+  """Test share_some_kmers method.
+  """
+  def test_share_some_kmers_nonmemoized(self):
+    np.random.seed(1)
+    args = { 'k': 5, 'num_kmers_to_test': 10,
+             'memoize_kmer_hashes': False }
+    a = probe.Probe.from_str('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    b = probe.Probe.from_str('ZYXWVUTSRQPONMLKJIHGFEDCBA')
+    c = probe.Probe.from_str('ABCXDEFGHIJKLMNOPQRATUVWYZ')
+    ab, ba, ac, ca = 0, 0, 0, 0
+    for i in xrange(100):
+      if a.shares_some_kmers(b, **args):
+        ab += 1
+      if b.shares_some_kmers(a, **args):
+        ba += 1
+      if a.shares_some_kmers(c, **args):
+        ac += 1
+      if c.shares_some_kmers(a, **args):
+        ca += 1
+    self.assertLess(ab, 10)
+    self.assertLess(ba, 10)
+    self.assertGreater(ac, 90)
+    self.assertGreater(ca, 90)
+
+  """Test share_some_kmers method.
+  """
+  def test_share_some_kmers_memoized(self):
+    np.random.seed(1)
+    args = { 'k': 5, 'num_kmers_to_test': 10,
+             'memoize_kmer_hashes': True }
+    a = probe.Probe.from_str('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    b = probe.Probe.from_str('ZYXWVUTSRQPONMLKJIHGFEDCBA')
+    c = probe.Probe.from_str('ABCXDEFGHIJKLMNOPQRATUVWYZ')
+    ab, ba, ac, ca = 0, 0, 0, 0
+    for i in xrange(100):
+      if a.shares_some_kmers(b, **args):
+        ab += 1
+      if b.shares_some_kmers(a, **args):
+        ba += 1
+      if a.shares_some_kmers(c, **args):
+        ac += 1
+      if c.shares_some_kmers(a, **args):
+        ca += 1
+    self.assertLess(ab, 10)
+    self.assertLess(ba, 10)
+    self.assertGreater(ac, 90)
+    self.assertGreater(ca, 90)
+
