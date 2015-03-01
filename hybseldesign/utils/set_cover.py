569b879d2ff20741166e716f23c58989dd00ecdf
Hayden Metsky <hmetsky@gmail.com>
2015-03-01 10:52:22 -0500

    Support weighted and partial set cover

--- hybseldesign/utils/set_cover.py
+++ hybseldesign/utils/set_cover.py
@@ -1,6 +1,15 @@
+"""Functions for working with instances of the set cover problem.
+"""
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+
 """Approximates the solution to an instance of the set cover problem
 using the well-known greedy algorithm.
 
+The following is a description of the problem and solution to the
+most basic case, in which sets are unweighted and we seek to cover
+the entire universe:
 We are given are universe U of (hashable) objects and a collection
 of m subsets S_1, S_2, ..., S_m of U whose union equals U. We wish
 to approximate the smallest number of these subets whose union is U
@@ -23,29 +32,78 @@ that it is NP-hard to approximate the problem to within c*ln(|U|)
 for any 0 < c < 1. Thus, this is a very good approximation given
 what is possible.
 
-The input 'sets' is a dict mapping set identifiers to sets.
+This generalizes the above case to support weighted, partial set
+cover. There are two primary changes to the above pseudocode that
+support this:
+ - Rather than looping while the universe is not covered, we instead
+   loop until we have covered as much as the universe as we desire
+   to cover (where that amount is determined by the parameter
+   indicating the fraction of the universe to cover). This change
+   alone supports 'partial cover'.
+ - Rather than picking the set S_i that covers the most of U, we
+   instead pick the set S_i that minimizes the ratio of the cost
+   of S_i (call it c_i) to the amount of the remaining universe, U,
+   that S_i covers. Let r be the number of elements that have yet
+   to be covered in order to obtain the desired partial cover; there
+   is no reason to favor a set that covers more than r. Thus, in
+   particular, we pick the set S_i that minimizes the quotient
+   [ c_i / min(r, | S_i \intersect U |) ].
+As shown by Petr Slavik in the paper "Improved performance of the
+greedy algorithm for the minimum set cover and minimum partial cover
+problems", the obtained solution is a ceil(ln(D))-approximation,
+where D is the cardinality of the largest subset. It also obtains a
+ceil(ln(p*|U|))-approximation where p is the fraction of the universe
+we wish to cover. When p=1, D <= |U| and therefore the
+ceil(ln(D))-approximation is the stronger one. But when p<1, it is
+possible that D > p*|U| and hence the ceil(ln(p*|U|))-approximation
+would be stronger. Note that in the weighted case, the solution
+seeks to minimize the sum of the weights of the chosen sets and
+the approximation is with respect to this.
+
+The input 'sets' is a dict mapping set identifiers to sets. The
+optional input 'costs' is a dict mapping set identifiers to the
+costs (or weights) of the set; the default is for every set to have
+a cost of 1, making this equivalent to the unweighted problem. The
+optional input 'p' is a float in [0,1] that specifies the fraction
+of the universe we must cover; the default is p=1, making this
+equivalent to the problem in which the entire universe is covered.
 The output is a set consisting of the identifiers of the sets chosen
-to be in the set cover. For example, if the input is
+to be in the set cover. For example, if the sets input is
   { 0: S_1, 1: S_2, 2: S_3 }
 where each S_i is a set, and the sets S_1 and S_3 are chosen to be
 in the set cover, then the output is {0,2}.
 """
+def approx(sets, costs=None, p=1.0):
+  if p < 0 or p > 1:
+    raise ValueError("p must be in [0,1]")
+  if costs == None:
+    # Give each set a default cost of 1
+    costs = { set_id: 1 for set_id in sets.keys() }
+  else:
+    for c in costs.values():
+      if c < 0:
+        raise ValueError("All costs must be nonnegative")
 
-__author__ = 'Hayden Metsky <hayden@mit.edu>'
-
-
-def approx(sets):
   # Create the universe as the union of all input sets
   universe = set()
   for s in sets.values():
     universe.update(s)
 
+  num_that_can_be_uncovered = int(len(universe) - p*len(universe))
+  # Above, use int(..) to take the floor. Also, expand out
+  # len(universe) rather than use int((1.0-p)*len(universe)) due to
+  # precision errors in Python -- e.g., int((1.0-0.8)*5) yields 0 on
+  # some machines.
+  num_left_to_cover = len(universe) - num_that_can_be_uncovered
+
   set_ids_not_in_cover = sets.keys()
   set_ids_in_cover = set()
-  # Keep iterating until universe is covered
-  while len(universe) > 0:
-    # Find the set the covers the most of universe
-    id_covering_most, most_num_covered = None, 0
+  # Keep iterating until desired partial cover is obtained
+  while num_left_to_cover > 0:
+    # Find the set that minimizes the ratio of its cost to the
+    # number of uncovered elements (that need to be covered) that
+    # it covers
+    id_min_ratio, min_ratio = None, float('inf')
     for id in set_ids_not_in_cover:
       # There is no strict need to keep track of set_ids_not_in_cover
       # and iterate through these; we could have iterated over
@@ -56,13 +114,22 @@ def approx(sets):
       # runtime improvements.
       s = sets[id]
       num_covered = len(s.intersection(universe))
-      if num_covered > most_num_covered:
-        id_covering_most = id
-        most_num_covered = num_covered
-    # id_covering_most goes into the set cover
-    set_ids_in_cover.add(id_covering_most)
-    set_ids_not_in_cover.remove(id_covering_most)
-    universe.difference_update(sets[id_covering_most])
+      # There is no need to cover more than num_left_to_cover
+      # elements
+      num_needed_covered = min(num_left_to_cover, num_covered)
+      if num_needed_covered == 0:
+        # s covers no elements that need to be covered, so it should
+        # not be in the set cover
+        continue
+      ratio = float(costs[id]) / num_needed_covered
+      if ratio < min_ratio:
+        id_min_ratio = id
+        min_ratio = ratio
+    # id_min_ratio goes into the set cover
+    set_ids_in_cover.add(id_min_ratio)
+    set_ids_not_in_cover.remove(id_min_ratio)
+    universe.difference_update(sets[id_min_ratio])
+    num_left_to_cover = len(universe) - num_that_can_be_uncovered
 
   return set_ids_in_cover
 
--- hybseldesign/utils/tests/test_set_cover.py
+++ hybseldesign/utils/tests/test_set_cover.py
@@ -12,7 +12,7 @@ from hybseldesign.utils import set_cover as sc
 """
 class TestSetCoverApprox(unittest.TestCase):
 
-  def test_basic(self):
+  def test_complete_unweighted(self):
     input = { 0: set([1,2]),
               1: set([1,2,4]),
               2: set([2,4]),
@@ -20,4 +20,110 @@ class TestSetCoverApprox(unittest.TestCase):
               4: set([3]) }
     desired_output = set([1,3,4])
     self.assertEqual(sc.approx(input), desired_output)
+
+  def test_partial_unweighted1(self):
+    input = { 0: set([1,2]),
+              1: set([1,2,4]),
+              2: set([2,4]),
+              3: set([4,5]),
+              4: set([3]) }
+    desired_output = set([1])
+    self.assertEqual(sc.approx(input, p=0.6), desired_output)
+
+  def test_partial_unweighted2(self):
+    input = { 0: set([1,2]),
+              1: set([1,2,4]),
+              2: set([2,4]),
+              3: set([4,5]),
+              4: set([2,3,6]) }
+    desired_output = set([1,4])
+    self.assertEqual(sc.approx(input, p=0.81), desired_output)
+
+  def test_complete_weighted1(self):
+    input = { 0: set([1,2]),
+              1: set([1,2,4]),
+              2: set([2,4]),
+              3: set([4,5]),
+              4: set([3]) }
+    costs = { 0: 2, 1: 1000, 2: 3, 3: 1, 4: 10 }
+    desired_output = set([0,3,4])
+    self.assertEqual(sc.approx(input, costs=costs), desired_output)
+
+  def test_complete_weighted2(self):
+    input = { 0: set([1,2]),
+              1: set([1,2,3,4,5]),
+              2: set([4]),
+              3: set([5]),
+              4: set([3]) }
+    costs = { 0: 2, 1: 1000, 2: 3, 3: 1, 4: 10 }
+    desired_output = set([0,2,3,4])
+    self.assertEqual(sc.approx(input, costs=costs), desired_output)
+
+  def test_partial_weighted1(self):
+    input = { 0: set([1,2]),
+              1: set([1,2,3,4,5]),
+              2: set([4]),
+              3: set([5]),
+              4: set([3]) }
+    costs = { 0: 2, 1: 1000, 2: 3, 3: 1, 4: 10 }
+    desired_output = set([3])
+    self.assertEqual(sc.approx(input, costs=costs, p=0.1),
+        desired_output)
+
+  def test_partial_weighted2(self):
+    input = { 0: set([1,2]),
+              1: set([2,3]),
+              2: set([4,5]),
+              3: set([5]),
+              4: set([4]) }
+    costs = { 0: 2, 1: 1000, 2: 100, 3: 10, 4: 10 }
+    desired_output = set([0,3,4])
+    self.assertEqual(sc.approx(input, costs=costs, p=0.7),
+        desired_output)
+
+  def test_partial_weighted3(self):
+    input = { 0: set([1,2]),
+              1: set([3]),
+              2: set([4]),
+              3: set([2,5]),
+              4: set([1]) }
+    costs = { 0: 2, 1: 1000, 2: 999, 3: 10, 4: 10 }
+    desired_output = set([0,2,3])
+    self.assertEqual(sc.approx(input, costs=costs, p=0.8),
+        desired_output)
+
+  def test_partial_weighted4(self):
+    input = { 0: set([1,2]),
+              1: set([3,4,5]),
+              2: set([3]),
+              3: set([4]),
+              4: set([5]) }
+    costs = { 0: 2.1, 1: 3, 2: 2, 3: 2, 4: 2 }
+    desired_output = set([1])
+    self.assertEqual(sc.approx(input, costs=costs, p=0.6),
+        desired_output)
+
+  def test_partial_weighted5(self):
+    input = { 0: set([1,2]),
+              1: set([2,3,4,5]),
+              2: set([3]),
+              3: set([4]),
+              4: set([5]) }
+    costs = { 0: 3, 1: 4, 2: 1, 3: 1, 4: 2 }
+    desired_output = set([1])
+    self.assertEqual(sc.approx(input, costs=costs, p=0.8),
+        desired_output)
+
+  def test_partial_weighted6(self):
+    input = { 0: set([1,2]),
+              1: set([2,3,4,5]),
+              2: set([3]),
+              3: set([4]),
+              4: set([5]) }
+    costs = { 0: 3, 1: 4.1, 2: 1, 3: 1, 4: 2 }
+    desired_output = set([0,2,3])
+    # The optimal solution is [1], but the approximation fails to
+    # find it
+    self.assertEqual(sc.approx(input, costs=costs, p=0.8),
+        desired_output)
 
