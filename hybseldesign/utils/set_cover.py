dd5332ba51f52c34f09ea1a06254909d6d541206
Hayden Metsky <hmetsky@gmail.com>
2015-03-02 12:03:13 -0500

    Implement set cover with multiple universes

--- hybseldesign/utils/set_cover.py
+++ hybseldesign/utils/set_cover.py
@@ -3,6 +3,8 @@
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
+import math
+
 
 """Approximates the solution to an instance of the set cover problem
 using the well-known greedy algorithm.
@@ -129,7 +131,149 @@ def approx(sets, costs=None, p=1.0):
     set_ids_in_cover.add(id_min_ratio)
     set_ids_not_in_cover.remove(id_min_ratio)
     universe.difference_update(sets[id_min_ratio])
-    num_left_to_cover = len(universe) - num_that_can_be_uncovered
+    num_left_to_cover = max(0,
+        len(universe) - num_that_can_be_uncovered)
 
   return set_ids_in_cover
 
+
+"""Approximates the solution to an instance of a version of the
+set cover problem in which there are multiple universes and we
+seek to find a collection of sets whose union covers a specified
+fraction of each given universe.
+
+The input 'sets' is a dict mapping set identifiers to sets. The
+input 'universes' is a dict mapping universe identifiers to sets
+consisting of the elements in the corresponding universe. The
+optional input 'costs' is a dict mapping set identifiers to the
+costs (or weights) of the set; the default is for every set to have
+a cost of 1, making this equivalent to the unweighted problem. The
+optional input 'universe_p' is a dict mapping universe identifiers
+to floats in [0,1] that specifiy the fraction of the corresponding
+universe we must cover; the default is for the coverage fraction of
+each universe to be 1.0, making this equivalent to the problem in
+which there is a single universe (the union of all the given
+universes) that must be completely covered.  The output is a set
+consisting of the identifiers of the sets chosen to be in the set
+cover. For example, if the sets input is
+  { 0: S_1, 1: S_2, 2: S_3 }
+where each S_i is a set, and the sets S_1 and S_3 are chosen to be
+in the set cover, then the output is {0,2}.
+
+This is a generalization of the partial, weighted set cover problem
+whose solution is approximated by approx(..). (For any input to
+that function, simply create a single universe whose elements
+consist of the union of all the input sets, and that supply that
+universe as input to this function.) Indeed, this function and
+approx(..) are very similar, with this one simply generalizing to
+allow for multiple universes. However, whereas approx(..) achieves
+a provable guarantee on the approximation factor, it is not clear
+whether the straightforward generalization implemented in this
+function achieves the same factor; therefore, there may be room for
+improvement in this function. Furthermore, the generalization to
+multiple universes will yield a slight increase in runtime (constant
+factors) compared to working directly with the more traditional
+notion of a single universe. For these reasons -- although the
+approx(..) function could be greatly shortened by simply calling
+this function -- we choose to implement the versions separately.
+"""
+def approx_multiuniverse(sets, universes, costs=None,
+    universe_p=None):
+  if costs == None:
+    # Give each set a default cost of 1
+    costs = { set_id: 1 for set_id in sets.keys() }
+  else:
+    for c in costs.values():
+      if c < 0:
+        raise ValueError("All costs must be nonnegative")
+  if universe_p == None:
+    # Give each universe a coverage fraction of 1.0 (i.e., cover
+    # all of it)
+    universe_p = { universe_id: 1 for universe_id in universes.keys() }
+  else:
+    for p in universe_p.values():
+      if p < 0 or p > 1:
+        raise ValueError(("The coverage fraction (p) of each "
+                          "universe must be in [0,1]"))
+    for universe_id in universes.keys():
+      if universe_id not in universe_p:
+        raise ValueError(("universe_p is missing a value for "
+                          "universe %d" % universe_id))
+
+  # Check that it is possible to achieve the desired coverage of
+  # each universe (i.e., by taking all the elements in the union of
+  # all sets)
+  sets_union = set()
+  for s in sets.values():
+    sets_union.update(s)
+  for universe_id, universe in universes.iteritems():
+    p = universe_p[universe_id]
+    num_possible = len(universe.intersection(sets_union))
+    num_desired = math.ceil(p*len(universe))
+    if num_possible < num_desired:
+      raise ValueError(("%d elements of universe %d must be "
+                        "covered, but it is only possible to cover "
+                        "%d elements" % (num_desired, universe_id,
+                        num_possible)))
+
+  # Make new sets of all the universes so they can be modified
+  # without disrupting the state of this function's caller
+  universes = { k: set(universes[k]) for k in universes.keys() }
+
+  num_that_can_be_uncovered = {}
+  num_left_to_cover = {}
+  for universe_id, universe in universes.iteritems():
+    p = universe_p[universe_id]
+    num_that_can_be_uncovered[universe_id] = \
+        int(len(universe) - p*len(universe))
+    # Above, use int(..) to take the floor. Also, expand out
+    # len(universe) rather than use int((1.0-p)*len(universe)) due to
+    # precision errors in Python -- e.g., int((1.0-0.8)*5) yields 0 on
+    # some machines.
+    num_left_to_cover[universe_id] = \
+        len(universe) - num_that_can_be_uncovered[universe_id]
+
+  set_ids_not_in_cover = sets.keys()
+  set_ids_in_cover = set()
+  # Keep iterating until desired partial cover of each universe
+  # is obtained (note that [] evaluates to False)
+  while [True for universe_id in universes.keys() \
+        if num_left_to_cover[universe_id] > 0]:
+    # Find the set that minimizes the ratio of its cost to the
+    # number of uncovered elements (that need to be covered) that
+    # it covers
+    id_min_ratio, min_ratio = None, float('inf')
+    for id in set_ids_not_in_cover:
+      # There is no strict need to keep track of set_ids_not_in_cover
+      # and iterate through these; we could have iterated over
+      # all input sets. However, sets that are already put into the
+      # set cover have zero intersection with any universe (i.e., cover
+      # none of it), so there is no reason to iterate over the sets
+      # already placed in the cover. Not doing so should yield some
+      # runtime improvements.
+      s = sets[id]
+      num_needed_covered_across_universes = 0
+      for universe_id, universe in universes.iteritems():
+        num_covered = len(s.intersection(universe))
+        # There is no need to cover more than num_left_to_cover
+        # elements for this universe
+        num_needed_covered = min(num_left_to_cover[universe_id],
+                                  num_covered)
+        num_needed_covered_across_universes += num_needed_covered
+      if num_needed_covered_across_universes == 0:
+        # s covers no elements that need to be covered, so it should
+        # not be in the set cover
+        continue
+      ratio = float(costs[id]) / num_needed_covered_across_universes
+      if ratio < min_ratio:
+        id_min_ratio = id
+        min_ratio = ratio
+    # id_min_ratio goes into the set cover
+    set_ids_in_cover.add(id_min_ratio)
+    set_ids_not_in_cover.remove(id_min_ratio)
+    for universe_id, universe in universes.iteritems():
+      universe.difference_update(sets[id_min_ratio])
+      num_left_to_cover[universe_id] = max(0,
+          len(universe) - num_that_can_be_uncovered[universe_id])
+
+  return set_ids_in_cover
--- hybseldesign/utils/tests/test_set_cover.py
+++ hybseldesign/utils/tests/test_set_cover.py
@@ -4,11 +4,12 @@
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
 import unittest
+import numpy as np
 
 from hybseldesign.utils import set_cover as sc
 
 
-"""Tests the set cover approximation implementation.
+"""Tests approx function.
 """
 class TestSetCoverApprox(unittest.TestCase):
 
@@ -109,17 +110,12 @@ class TestSetCoverApprox(unittest.TestCase):
               2: set([3]),
               3: set([4]),
               4: set([5]) }
+
     costs = { 0: 3, 1: 4, 2: 1, 3: 1, 4: 2 }
     desired_output = set([1])
     self.assertEqual(sc.approx(input, costs=costs, p=0.8),
         desired_output)
 
-  def test_partial_weighted6(self):
-    input = { 0: set([1,2]),
-              1: set([2,3,4,5]),
-              2: set([3]),
-              3: set([4]),
-              4: set([5]) }
     costs = { 0: 3, 1: 4.1, 2: 1, 3: 1, 4: 2 }
     desired_output = set([0,2,3])
     # The optimal solution is [1], but the approximation fails to
@@ -137,3 +133,232 @@ class TestSetCoverApprox(unittest.TestCase):
     input = { 0: set([1]) }
     self.assertEqual(sc.approx(input), set([0]))
 
+
+"""Tests approx_multiuniverse function.
+"""
+class TestSetCoverApproxMultiuniverse(unittest.TestCase):
+
+  def test_one_universe_complete_unweighted(self):
+    sets = { 0: set([1,2]),
+             1: set([1,2,4]),
+             2: set([2,4]),
+             3: set([4,5]),
+             4: set([3]) }
+    universes = { 0: set([1,2,3,4,5]) }
+    desired_output = set([1,3,4])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes),
+        desired_output)
+
+  def test_two_universes_complete_unweighted(self):
+    sets = { 0: set([1,2]),
+             1: set([1,2,4]),
+             2: set([2,4]),
+             3: set([4,5]),
+             4: set([3]) }
+
+    universes = { 0: set([1,2,4]), 1: set([3,5]) }
+    desired_output = set([1,3,4])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes),
+        desired_output)
+
+    universes = { 0: set([1,2,4]) }
+    desired_output = set([1])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes),
+        desired_output)
+
+  def test_impossible_to_cover(self):
+    sets = { 0: set([1,2]),
+             1: set([1,2,4]),
+             2: set([2,4]),
+             3: set([4,5]),
+             4: set([3]) }
+    universes = { 0: set([1,2,6]), 1: set([3,5]) }
+    self.assertRaises(ValueError,
+        sc.approx_multiuniverse, sets, universes)
+
+  def test_one_universe_partial_unweighted(self):
+    sets = { 0: set([1,2]),
+             1: set([1,2,4]),
+             2: set([2,4]),
+             3: set([4,5]),
+             4: set([3]) }
+    universes = { 0: set([1,2,3,4,5]) }
+    universe_p = { 0: 0.6 }
+    desired_output = set([1])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        universe_p=universe_p), desired_output)
+
+  def test_two_universes_partial_unweighted(self):
+    sets = { 0: set([1,2]),
+             1: set([1,2,4]),
+             2: set([2,4]),
+             3: set([4,5]),
+             4: set([3]) }
+
+    universes = { 0: set([3,5]), 1: set([1,2,4]) }
+    universe_p = { 0: 1.0, 1: 0.3 }
+    desired_output = set([3,4])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        universe_p=universe_p), desired_output)
+
+    universes = { 0: set([2,3,4]), 1: set([1,5]) }
+    universe_p = { 0: 1.0, 1: 0.5 }
+    desired_output = set([1,4])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        universe_p=universe_p), desired_output)
+
+  def test_two_universes_partial_weighted1(self):
+    sets = { 0: set([1,2]),
+             1: set([1,2,3,4,5]),
+             2: set([4]),
+             3: set([5]),
+             4: set([3]) }
+
+    universes = { 0: set([1,2]), 1: set([3,4,5]) }
+    costs = { 0: 2, 1: 1000, 2: 3, 3: 1, 4: 10 }
+    universe_p = { 0: 0.1, 1: 0.1 }
+    desired_output = set([0,3])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+    universes = { 0: set([1,2]), 1: set([3,4,5]) }
+    costs = { 0: 2, 1: 1000, 2: 3, 3: 1, 4: 10 }
+    universe_p = { 0: 0.0, 1: 0.1 }
+    desired_output = set([3])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+    universes = { 0: set([1,2]), 1: set([3,4,5]) }
+    costs = { 0: 2, 1: 1000, 2: 3, 3: 1, 4: 10 }
+    universe_p = { 0: 0.5, 1: 0.5 }
+    desired_output = set([0,2,3])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+  def test_two_universes_partial_weighted2(self):
+    sets = { 0: set([1,2]),
+             1: set([2,3,4,5]),
+             2: set([3]),
+             3: set([4]),
+             4: set([5]) }
+
+    universes = { 0: set([1,2,3]), 1: set([4,5]) }
+    costs = { 0: 3, 1: 4, 2: 1, 3: 1, 4: 2 }
+    universe_p = { 0: 1.0, 1: 0.5 }
+    desired_output = set([0,2,3])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+    universes = { 0: set([3,4,5]), 1: set([1,2]) }
+    costs = { 0: 1000, 1: 4, 2: 1, 3: 1, 4: 2 }
+    universe_p = { 0: 0.6, 1: 0.5 }
+    desired_output = set([1,2,3])
+    # The optimal solution is [1] but the approximation fails to
+    # find it
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+    universes = { 0: set([3,4,5]), 1: set([1,2]) }
+    costs = { 0: 1000, 1: 4, 2: 1.5, 3: 1.5, 4: 2 }
+    universe_p = { 0: 0.6, 1: 0.5 }
+    desired_output = set([1])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+  def test_three_universes_partial_weighted(self):
+    sets = { 0: set([1,2]),
+             1: set([2,3,4]),
+             2: set([3]),
+             3: set([4,6]),
+             4: set([5]) }
+    universes = { 0: set([1,2]), 1: set([3,4]), 2: set([5,6]) }
+    costs = { 0: 3, 1: 4, 2: 1, 3: 1, 4: 1000 }
+    universe_p = { 0: 0.5, 1: 0.5, 2: 1.0 }
+    desired_output = set([0,3,4])
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        costs, universe_p), desired_output)
+
+  def test_empty_universe(self):
+    sets = { 0: set([1,2]) }
+    universes = { 0: set([1]), 1: set() }
+    universe_p = { 0: 1.0, 1: 1.0 }
+    self.assertEqual(sc.approx_multiuniverse(sets, universes,
+        universe_p=universe_p), set([0]))
+
+  """Verifies that a computed set cover achieves the desired
+  coverage of each universe.
+  """
+  def verify_partial_cover(self, sets, universes, universe_p,
+      output):
+    covered_elements = set()
+    for set_id in output:
+      covered_elements.update(sets[set_id])
+    for universe_id, universe in universes.iteritems():
+      desired_p = universe_p[universe_id]
+      desired_num_covered = desired_p * len(universe)
+      num_covered = len(covered_elements.intersection(universe))
+      self.assertGreaterEqual(num_covered, desired_num_covered)
+
+  """Returns the ratio of the sum of the weights of the sets in a
+  computed set cover to the sum of the weights of all the sets.
+  """
+  def weight_frac(self, costs, output):
+    sum_of_all_weights = np.sum(costs.values())
+    sum_of_weights_of_selected_sets = \
+        np.sum([costs[set_id] for set_id in output])
+    return float(sum_of_weights_of_selected_sets) / sum_of_all_weights
+
+  """Generates random instances of set cover, computes the solution,
+  and verifies that the solution achieves the desired coverage. Also
+  verifies that, on average, the solution achieves a reasonable
+  reduction in the sum of weights of chosen sets versus choosing
+  all sets.
+  """
+  def test_random(self):
+    np.random.seed(1)
+    weight_fracs = []
+    for n in xrange(25):
+      # Generate the universes
+      num_universes = np.random.randint(1, 10)
+      universes = {}
+      universe_p = {}
+      universe_union = set()
+      max_of_previous_universe = 0
+      for universe_id in xrange(num_universes):
+        universe_size = np.random.randint(100, 500)
+        els = set(np.random.randint(max_of_previous_universe+1,
+                  max_of_previous_universe+500, size=universe_size))
+        universes[universe_id] = els
+        max_of_previous_universe = max(els)
+        universe_union.update(els)
+      universe_union_list = list(universe_union)
+      # Generate the sets
+      num_sets = np.random.randint(500, 1000)
+      sets = {}
+      sets_union = set()
+      for set_id in xrange(num_sets):
+        set_size = np.random.randint(10, 250)
+        els = set(np.random.choice(universe_union_list,
+                  size=set_size, replace=False))
+        sets[set_id] = els
+        sets_union.update(els)
+      # Remove from all universes any elements that don't show
+      # up in a set in order to ensure that all universes can be
+      # covered fully
+      for universe in universes.values():
+        universe.intersection_update(sets_union)
+      # Generate random set costs and random coverage fractions
+      costs = { set_id: 1.0+10.0*np.random.random() \
+                for set_id in xrange(num_sets) }
+      universe_p = { universe_id: np.random.random() \
+                      for universe_id in xrange(num_universes) }
+      # Compute the set cover
+      output = sc.approx_multiuniverse(sets, universes, costs,
+                universe_p)
+      self.verify_partial_cover(sets, universes, universe_p,
+          output)
+      weight_fracs += [self.weight_frac(costs, output)]
+    # There's no guarantee that the average weight_frac should be
+    # small, but in the average case it should be so test it anyway
+    # (e.g., test that it's less than 0.01)
+    self.assertLess(np.median(weight_fracs), 0.01)
