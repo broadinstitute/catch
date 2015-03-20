60834560ba1e057b9e305dfb1182da828543a7d7
Hayden Metsky <hmetsky@gmail.com>
2015-03-20 17:16:09 -0400

    Add interval scheduling to interval module

--- hybseldesign/utils/tests/test_interval.py
+++ hybseldesign/utils/tests/test_interval.py
@@ -38,3 +38,32 @@ class TestMergeOverlapping(unittest.TestCase):
     self.compare([(1,5), (3,5), (5,8), (10,12), (15,18), (17,20)],
                   [(1,8), (10,12), (15,20)])
 
+"""Tests the schedule function.
+"""
+class TestSchedule(unittest.TestCase):
+
+  def compare(self, input, desired_output):
+    self.assertEqual(interval.schedule(input),
+      desired_output)
+
+  def test_single(self):
+    self.compare([((1,2), 1)], [1])
+
+  def test_two_nonoverlapping(self):
+    self.compare([((1,2), 1), ((4,5), 2)], [1,2])
+    self.compare([((4,5), 2), ((1,2), 1)], [1,2])
+
+  def test_two_overlapping(self):
+    self.compare([((1,3), 1), ((2,5), 2)], [1])
+    self.compare([((2,5), 2), ((1,3), 1)], [1])
+
+  def test_three(self):
+    self.compare([((1,5), 1), ((3,7), 2), ((9,12), 3)], [1,3])
+
+  def test_within(self):
+    self.compare([((1,5), 1), ((3,4), 2), ((6,8), 3)], [2,3])
+
+  def test_loner_in_middle(self):
+    self.compare([((1,5), 1), ((3,4), 2), ((5,8), 3), ((10,12), 4),
+                  ((15,18),5), ((17,20), 6)], [2,3,4,5])
+
