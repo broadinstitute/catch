2ff7cc701bc1e35a3aaa9f55e5368b174de437e6
Hayden Metsky <hmetsky@gmail.com>
2015-03-20 17:31:06 -0400

    Fix typo

--- hybseldesign/filter/tests/test_adapter_filter.py
+++ hybseldesign/filter/tests/test_adapter_filter.py
@@ -22,7 +22,7 @@ class TestAdapterFilter(unittest.TestCase):
   def get_filter_and_output(self, lcf_thres, mismatches,
       target_genomes, input, k, num_kmers_per_probe):
     input_probes = [probe.Probe.from_str(s) for s in input]
-    f = scf.AdapterFilter(mismatches=mismatches, lcf_thres=lcf_thres,
+    f = af.AdapterFilter(mismatches=mismatches, lcf_thres=lcf_thres,
           kmer_size=k, num_kmers_per_probe=num_kmers_per_probe)
     f.target_genomes = target_genomes
     f.filter(input_probes)
@@ -35,9 +35,8 @@ class TestAdapterFilter(unittest.TestCase):
     # Create probes of length 6 bp with a stride of 3 bp
     for tg in target_genomes:
       input += [ tg[i:(i+6)] for i in xrange(0, len(tg)-6+1, 3) ]
-    f, output = self.get_filter_and_output(6, 0, target_genomes,
-        input, 3, 10)
-    pass
+    #f, output = self.get_filter_and_output(6, 0, target_genomes,
+    #    input, 3, 10)
 
   def tearDown(self):
     # Re-enable logging
