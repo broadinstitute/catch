1f2dc03efe3208f7952fcb358d6b3fd76d99276b
Hayden Metsky <hmetsky@gmail.com>
2015-04-28 22:33:38 -0400

    Add Measles dataset

---
+++ hybseldesign/datasets/measles.py
@@ -0,0 +1,14 @@
+"""Dataset with Measles sequences.
+
+A dataset with 53 Measles genomes.
+"""
+
+import sys
+
+from hybseldesign.datasets import GenomesDatasetSingleChrom
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+ds = GenomesDatasetSingleChrom(__name__, __file__)
+ds.set_fasta_path("data/measles.fasta", relative=True)
+sys.modules[__name__] = ds
