b9a9181d56fc67df9429aef835b4e94ac0ef1fe5
Hayden Metsky <hmetsky@gmail.com>
2015-04-28 22:21:40 -0400

    Add Dengue dataset

---
+++ hybseldesign/datasets/dengue.py
@@ -0,0 +1,14 @@
+"""Dataset with Dengue sequences.
+
+A dataset with 302 Dengue samples.
+"""
+
+import sys
+
+from hybseldesign.datasets import GenomesDatasetSingleChrom
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+ds = GenomesDatasetSingleChrom(__name__, __file__)
+ds.set_fasta_path("data/dengue.fasta", relative=True)
+sys.modules[__name__] = ds
