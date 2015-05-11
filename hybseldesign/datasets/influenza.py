126025705dd52b4115e8e3bee749620af3a7fe47
Hayden Metsky <hmetsky@gmail.com>
2015-05-11 18:55:58 -0400

    Split influenza dataset into genomes
    Each FASTA file corresponds to a separate genome. Most have 8
    segments each, although some genomes are incomplete and therefore
    have fewer than 8 segments. In total, 2 segments are left out
    compared to the previous dataset because they could not be
    matched with a genome.

--- hybseldesign/datasets/influenza.py
+++ hybseldesign/datasets/influenza.py
@@ -1,15 +1,31 @@
 """Dataset with Influenza sequences.
 
-A dataset with 8985 Influenza segments (both Influenza A and
-Influenza B).
+A dataset with 1125 Influenza genomes, with a total of 8983 segments
+(both Influenza A and Influenza B); some are incomplete.
 """
 
+from os.path import dirname
+from os.path import join
+from os import listdir
 import sys
 
-from hybseldesign.datasets import GenomesDatasetSingleChrom
+from hybseldesign.datasets import GenomesDatasetMultiChrom
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
-ds = GenomesDatasetSingleChrom(__name__, __file__)
-ds.set_fasta_path("data/influenza.fasta", relative=True)
+
+def seq_header_to_chr(header):
+    import re
+    c = re.compile(r'Segment:([0-9]+)')
+    m = c.search(header)
+    if not m:
+        raise ValueError("Unknown segment in header %s" % header)
+    return "segment_" + str(m.group(1))
+
+chrs = ["segment_" + str(i) for i in range(1, 9)]
+ds = GenomesDatasetMultiChrom(__name__, __file__, chrs, seq_header_to_chr)
+
+for f in listdir(join(dirname(__file__), "data/influenza/")):
+    ds.add_fasta_path("data/influenza/" + f, relative=True)
+
 sys.modules[__name__] = ds
