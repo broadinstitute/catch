7189fb218c86ebb658f142a514d4ff5248c34387
Hayden Metsky <hmetsky@gmail.com>
2015-05-11 18:55:28 -0400

    Modify hg19 to support reading chr from sequence header

--- hybseldesign/datasets/hg19.py
+++ hybseldesign/datasets/hg19.py
@@ -7,6 +7,10 @@ from hybseldesign.datasets import GenomesDatasetMultiChrom
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
+
+def seq_header_to_chr(header):
+    return header
+
 chrs = [str(x) for x in range(1, 23)] + ["X", "Y", "MT"]
-ds = GenomesDatasetMultiChrom(__name__, __file__, chrs)
+ds = GenomesDatasetMultiChrom(__name__, __file__, chrs, seq_header_to_chr)
 sys.modules[__name__] = ds
