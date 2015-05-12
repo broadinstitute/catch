29ce55c5b4e5f68bbbdca7ef202d09ce7ce43af9
Hayden Metsky <hmetsky@gmail.com>
2015-05-12 16:26:24 -0400

    Add five datasets
    Adds genome datasets for:
     - Hepatitis C
     - MERS
     - Rhabdoviruses
     - SARS
     - Yellow fever virus

---
+++ hybseldesign/datasets/rhabdovirus.py
@@ -0,0 +1,14 @@
+"""Dataset with Rhabdovirus sequences.
+
+A dataset with 243 Rhabdovirus genomes.
+"""
+
+import sys
+
+from hybseldesign.datasets import GenomesDatasetSingleChrom
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+ds = GenomesDatasetSingleChrom(__name__, __file__)
+ds.set_fasta_path("data/rhabdovirus.fasta", relative=True)
+sys.modules[__name__] = ds
---
+++ hybseldesign/datasets/sars.py
@@ -0,0 +1,14 @@
+"""Dataset with SARS sequences.
+
+A dataset with 143 SARS genomes.
+"""
+
+import sys
+
+from hybseldesign.datasets import GenomesDatasetSingleChrom
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+ds = GenomesDatasetSingleChrom(__name__, __file__)
+ds.set_fasta_path("data/sars.fasta", relative=True)
+sys.modules[__name__] = ds
