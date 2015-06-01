229d86868fca44cd18177a03e72d5068f51b1853
Hayden Metsky <hmetsky@gmail.com>
2015-06-01 11:00:17 -0400

    Update HIV dataset
    Replace the single HIV dataset with two HIV datasets: one
    corresponding to HIV-1 and the other corresponding to
    HIV-2. Also, for each of these, add a dataset consisting
    of the same sequences, except that the LTRs are stripped
    from both ends of each sequence.

---
+++ hybseldesign/datasets/hiv1.py
@@ -0,0 +1,14 @@
+"""Dataset with HIV-1 sequences.
+
+A dataset with 1749 HIV-1 genomes.
+"""
+
+import sys
+
+from hybseldesign.datasets import GenomesDatasetSingleChrom
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+ds = GenomesDatasetSingleChrom(__name__, __file__)
+ds.set_fasta_path("data/hiv1.fasta", relative=True)
+sys.modules[__name__] = ds
