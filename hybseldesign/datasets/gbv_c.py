c7f425b851e14efebbf10ca8b9cbe7beb6dea82e
Hayden Metsky <hmetsky@gmail.com>
2015-06-10 16:49:18 -0400

    Add GB virus C dataset

---
+++ hybseldesign/datasets/gbv_c.py
@@ -0,0 +1,15 @@
+"""Dataset with GB virus C sequences.
+
+A dataset with 17 GB virus C (GBV-C, formerly known as hepatitis
+G virus) genomes.
+"""
+
+import sys
+
+from hybseldesign.datasets import GenomesDatasetSingleChrom
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+ds = GenomesDatasetSingleChrom(__name__, __file__)
+ds.set_fasta_path("data/gbv_c.fasta", relative=True)
+sys.modules[__name__] = ds
