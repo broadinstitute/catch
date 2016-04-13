427ecd16b9c55f5d5f0c6306958ccc403a4e5a14
Hayden Metsky <hmetsky@gmail.com>
2016-04-13 17:36:14 -0400

    Limit the number of 'N' bases expanded in a probe
    This change randomly selects some 'N' bases to "expand" and
    randomly fixes the rest at unambiguous bases. This is needed
    because some probes (e.g., in the hiv1_without_ltr) dataset
    have up to 13 'N' bases; without this change, these would
    be expanded to 4^13 probes.

--- hybseldesign/filter/n_expansion_filter.py
+++ hybseldesign/filter/n_expansion_filter.py
@@ -21,7 +21,17 @@ The number of output probes is dependent on the number of 'N'
 bases within each probe. The order of the input probes is
 conserved -- i.e., the new, "expanded" probes are returned among
 the other probes.
+
+Since the number of output probes grows exponentially in the
+number of 'N' bases within a probe, we can limit this with
+the 'limit_n_expansion_randomly' parameter. When set to a nonnegative
+integer, only limit_n_expansion_randomly 'N' bases are expanded;
+these ones are chosen randomly. The other 'N' bases, if there
+exist others, are randomly replaced with an unambiguous base.
+When set to None, all 'N' bases are expanded.
 """
+
+import random
 
 from hybseldesign.filter.base_filter import BaseFilter
 from hybseldesign import probe
@@ -33,6 +43,17 @@ class NExpansionFilter(BaseFilter):
     """Filter that expands 'N' bases within probes.
     """
 
+    def __init__(self, limit_n_expansion_randomly=3):
+        """
+        Args:
+            limit_n_expansion_randomly: when set to a nonnegative integer,
+                only this number of 'N' bases are expanded, and they
+                are randomly chosen; the rest of are replaced with
+                random unambiguous bases. When None, all 'N' bases
+                are expanded
+        """
+        self.limit_n_expansion_randomly = limit_n_expansion_randomly
+
     def _filter(self, input):
         """Return input probes where 'N' bases are replaced with real bases.
         """
@@ -40,12 +61,28 @@ class NExpansionFilter(BaseFilter):
 
         output = []
         for p in input:
-            if 'N' not in p.seq_str:
+            num_n = p.seq_str.count('N')
+            if num_n == 0:
                 # p has no 'N' bases, so there is nothing to expand
                 output += [p]
                 continue
 
-            expanded_probe_seqs = [p.seq_str]
+            p_seq_init = p.seq_str
+            if (self.limit_n_expansion_randomly is not None and
+                    num_n > self.limit_n_expansion_randomly):
+                # Randomly replace (num_n - self.limit_n_expansion_randomly)
+                # 'N' bases with random unambiguous bases
+                occurrences = [i for i, base in enumerate(p_seq_init)
+                               if base == 'N']
+                p_seq_init_list = list(p_seq_init)
+                while len(occurrences) > self.limit_n_expansion_randomly:
+                    occ_to_replace = random.choice(occurrences)
+                    replacement = random.choice(real_bases)
+                    p_seq_init_list[occ_to_replace] = replacement
+                    occurrences.remove(occ_to_replace)
+                p_seq_init = ''.join(p_seq_init_list)
+
+            expanded_probe_seqs = [p_seq_init]
             # Keep iterating (expanding) while there are still 'N'
             # bases left
             while [s for s in expanded_probe_seqs if 'N' in s]:
