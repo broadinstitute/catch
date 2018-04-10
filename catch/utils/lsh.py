5733405334b0584df5d0e843726f585ddc8af454
Hayden Metsky <hmetsky@gmail.com>
2018-04-09 22:02:16 -0400

    Add implementation of MinHash
    This adds another family of hash functions to use for LSH. In this context,
    each sequence can be thought of as a set of k-mers; hash functions from this
    family will hash similar sequences to the same value based on their shared
    k-mers.

--- catch/utils/lsh.py
+++ catch/utils/lsh.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import logging
 import math
 import random
+import zlib
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
@@ -41,6 +42,80 @@ class HammingDistanceFamily:
             to the same value if they are within dist of each other
         """
         return 1.0 - float(dist) / float(self.dim)
+
+
+class MinHashFamily:
+    """An LSH family that works by taking the minimum permutation of
+    k-mers in a string/sequence (MinHash).
+
+    See (Broder et al. 1997) and (Andoni and Indyk 2008) for details.
+    """
+
+    def __init__(self, kmer_size):
+        self.kmer_size = kmer_size
+
+    def make_h(self):
+        """Construct a random hash function for this family.
+
+        Here, we treat a sequence as being a set of k-mers. We calculate
+        a hash value for each k-mer and the hash function on the sequence
+        returns the minimum of these.
+
+        Returns:
+            hash function
+        """
+        # First construct a random hash function for a k-mer that
+        # is a universal hash function (effectively a "permutation"
+        # of the k-mer)
+        # Constrain all values to be in [0, 2^31 - 1] to have a bound
+        # on the output of the universal hash function; this upper bound
+        # is nice because it is also a prime, so we can simply work
+        # modulo (2^31 - 1)
+        p = 2**31 - 1
+        # Let the random hash function be:
+        #   (a*x + b) mod p
+        # for random integers a, b (a in [1, p] and b in [0, p])
+        a = random.randint(1, p)
+        b = random.randint(0, p)
+        def kmer_hash(x):
+            # Hash a k-mer x with the random hash function
+            # hash(..) uses a random seed in Python 3.3+, so its output
+            # varies across Python processes; use zlib.adler32(..) for a
+            # deterministic hash value of the k-mer
+            x_hash = zlib.adler32(x.encode('utf-8'))
+            return (a * x_hash + b) % p
+
+        def h(s):
+            # For a string/sequence s, have the MinHash function be the minimum
+            # hash over all the k-mers in it
+            assert self.kmer_size <= len(s)
+            if self.kmer_size >= len(s) / 2:
+                logger.warning(("The k-mer size %d is large (> (1/2)x) "
+                    "compared to the size of a sequence to hash (%d), which "
+                    "might make it difficult for MinHash to find similar "
+                    "sequence"))
+            kmer_hashes = []
+            for i in range(len(s) - self.kmer_size + 1):
+                kmer = s[i:(i + self.kmer_size)]
+                kmer_hashes += [kmer_hash(kmer)]
+            return min(kmer_hashes)
+        return h
+
+    def P1(self, dist):
+        """Calculate lower bound on probability of collision for nearby sequences.
+
+        Args:
+            dist: Jaccard distance (1 minus Jaccard similarity); suppose
+                two sequences are within this distance of each other. The
+                Jaccard similarity can be thought of as the overlap in k-mers
+                between the two sequences
+
+        Returns:
+            lower bound on probability that two sequences (e.g., probes) hash
+            to the same value if they are within dist of each other
+        """
+        # With MinHash, the collision probability is the Jaccard similarity
+        return 1.0 - dist
 
 
 class HashConcatenation:
