524541e981ec8e4345b782ca75a5f8b1d94bdc41
Hayden Metsky <hmetsky@gmail.com>
2019-02-25 16:45:43 -0500

    Cluster sequences, if desired, and run filters on each cluster
    This commit updates ProbeDesigner to cluster genomes, if desired, and
    design probes separately for each cluster. Specifically, it clusters
    sequences to create one genome per sequence. Then, for each cluster, it
    creates candidate probes and runs the filters that should be run
    separately on each cluster. Finally, it merges the designed probes from
    each cluster and runs the remaining filters on the combined output
    probes.

--- catch/filter/probe_designer.py
+++ catch/filter/probe_designer.py
@@ -4,6 +4,8 @@
 import logging
 
 from catch.filter import candidate_probes
+from catch import genome
+from catch.utils import cluster
 
 __author__ = 'Hayden Metsky <hayden@mit.edu>'
 
@@ -17,7 +19,8 @@ class ProbeDesigner:
     """
 
     def __init__(self, genomes, filters, probe_length,
-            probe_stride, allow_small_seqs=None, seq_length_to_skip=None):
+            probe_stride, allow_small_seqs=None, seq_length_to_skip=None,
+            cluster_threshold=None, cluster_merge_after=None):
         """
         Args:
             genomes: list [g_1, g_2, g_m] of m groupings of genomes, where
@@ -35,6 +38,15 @@ class ProbeDesigner:
             seq_length_to_skip: if set, skip sequences whose length is <=
                 the given value (i.e., do not design candidate probes for
                 them)
+            cluster_threshold: if set, cluster genomes and run filters (except
+                the final ones) separately on each cluster, and merge probes;
+                this value is the inter-cluster distance to merge clusters,
+                in average nucleotide dissimilarity (1-ANI, where ANI is
+                average nucleotide identity); higher results in fewer clusters
+            cluster_merge_after: a filter in filters such that output probes
+                from each cluster are merged just after running this filter,
+                and all subsequent filters are run on the merged list; must be
+                set if cluster_threshold is set
         """
         self.genomes = genomes
         self.filters = filters
@@ -42,28 +54,121 @@ class ProbeDesigner:
         self.probe_stride = probe_stride
         self.allow_small_seqs = allow_small_seqs
         self.seq_length_to_skip = seq_length_to_skip
+        self.cluster_threshold = cluster_threshold
+        self.cluster_merge_after = cluster_merge_after
 
-    def design(self):
-        """Design probes using the provided filters.
+    def _cluster_genomes(self):
+        """Cluster genomes by nucleotide similarity using MinHash signatures.
 
-        Generates the set of candidate probes and runs these through the
-        filters. Stores the candidate probes in self.candidate_probes and
-        the probes processed by the filters in self.final_probes.
+        This collapses all sequences, across both groups and genomes, into one
+        list of sequences in one group. Therefore, genomes will not be grouped
+        as specified in the input and sequences will not be grouped by genome.
+
+        Returns:
+            list of Genome objects, where each consists of a single sequence
         """
-
-        logger.info("Building candidate probes from target sequences")
-        self.candidate_probes = []
+        if len(self.genomes) > 1:
+            logger.warning(("There are >1 groups of genomes in the input, but "
+                "clustering these will result in just one group; differential "
+                "identification or other tasks that rely on group separation "
+                "will no longer work"))
+        seqs = {}
+        seq_idx = 0
         for genomes_from_group in self.genomes:
             for g in genomes_from_group:
-                self.candidate_probes += candidate_probes.\
+                for s in g.seqs:
+                    seqs[seq_idx] = s
+                    seq_idx += 1
+
+        logger.info(("Clustering %d sequences using MinHash signatures"),
+                seq_idx)
+        clusters = cluster.cluster_with_minhash_signatures(seqs,
+                threshold=self.cluster_threshold)
+
+        clustered_genomes = []
+        for clust in clusters:
+            genomes_in_clust = []
+            for seq_idx in clust:
+                seq = seqs[seq_idx]
+                genomes_in_clust += [genome.Genome.from_one_seq(seq)]
+            clustered_genomes += [genomes_in_clust]
+        return clustered_genomes
+
+    def _design_for_genomes(self, genomes, filters):
+        """Design probes on a subset of genomes using a subset of filters.
+
+        Generates the set of candidate probes and runs these through the
+        filters.
+
+        Args:
+            genomes: list [g_1, g_2, g_m] of m groupings of genomes, where
+                each g_i is a list of genome.Genomes belonging to group i.
+                For example, a group may be a species and each g_i would be
+                a list of the target genomes of species i.
+            filters: an (ordered) list of filters, each of which should be
+                an instance of a subclass of BaseFilter
+
+        Returns:
+            tuple (candidate_probes, output_probes) where candidate_probes is a
+            list of candidate probes and output_probes is the list of candidate
+            probes after passing through the provided filters
+        """
+        logger.info("Building candidate probes from target sequences")
+        candidates = []
+        for genomes_from_group in genomes:
+            for g in genomes_from_group:
+                candidates += candidate_probes.\
                     make_candidate_probes_from_sequences(
                         g.seqs, probe_length=self.probe_length,
                         probe_stride=self.probe_stride,
                         allow_small_seqs=self.allow_small_seqs,
                         seq_length_to_skip=self.seq_length_to_skip)
 
-        probes = self.candidate_probes
-        for f in self.filters:
+        probes = candidates
+        for f in filters:
+            logger.info("Starting filter %s", f.__class__.__name__)
+            f.target_genomes = genomes
+            probes = f.filter(probes)
+        return (candidates, probes)
+
+    def design(self):
+        """Design probes using the provided filters.
+
+        If desired, this first clusters sequences and then designs separately
+        for each cluster. This stores the resulting probes in
+        self.final_probes and all the candidate probes in
+        self.candidate_probes.
+        """
+        if self.cluster_threshold is None:
+            candidates, probes = self._design_for_genomes(self.genomes,
+                    self.filters)
+            self.candidate_probes = candidates
+            self.final_probes = probes
+            return
+
+        # Find filters before and after merging
+        assert self.cluster_merge_after is not None
+        assert self.cluster_merge_after in self.filters
+        filter_merge_idx = self.filters.index(self.cluster_merge_after) + 1
+        filters_before_merge = self.filters[:filter_merge_idx]
+        filters_after_merge = self.filters[filter_merge_idx:]
+
+        # Cluster genomes and design probes on each cluster
+        clustered_genomes = self._cluster_genomes()
+        candidates = []
+        probes = set()
+        for genomes in clustered_genomes:
+            candidates_for_cluster, probes_for_cluster = \
+                    self._design_for_genomes([genomes], filters_before_merge)
+            candidates += candidates_for_cluster
+            probes |= set(probes_for_cluster)
+
+        self.candidate_probes = candidates
+
+        # Run the remaining filters (filters_after_merge) on all the probes
+        # after the merge
+        probes = list(probes)
+        for f in filters_after_merge:
             logger.info("Starting filter %s", f.__class__.__name__)
             f.target_genomes = self.genomes
             probes = f.filter(probes)
--- catch/filter/tests/test_probe_designer.py
+++ catch/filter/tests/test_probe_designer.py
@@ -18,7 +18,7 @@ class TestProbeDesigner(unittest.TestCase):
 
     def setUp(self):
         # Disable logging
-        logging.disable(logging.INFO)
+        logging.disable(logging.WARNING)
 
     def test_one_filter1(self):
         """A basic test with a duplicate filter and one input sequence.
@@ -119,6 +119,74 @@ class TestProbeDesigner(unittest.TestCase):
         pb.design()
         self.assertEqual(pb.candidate_probes, desired_candidate_probes)
         self.assertEqual(pb.final_probes, desired_final_probes)
+
+    def tearDown(self):
+        # Re-enable logging
+        logging.disable(logging.NOTSET)
+
+
+class TestProbeDesignerWithClustering(unittest.TestCase):
+    """Tests the probe designer output, including cluster sequences, on
+    contrived input.
+    """
+
+    def setUp(self):
+        # Disable logging
+        logging.disable(logging.WARNING)
+
+    def test_cluster_genomes(self):
+        """Test the _cluster_genomes() function.
+        """
+        g1 = genome.Genome.from_one_seq('ATTA'*500)
+        g2 = genome.Genome.from_one_seq('TAAT'*500)
+        g3 = genome.Genome.from_one_seq('GC'*500)
+        g4 = genome.Genome.from_one_seq('CG'*500)
+        seqs = [[g1, g3], [g2, g4]]
+        pb = probe_designer.ProbeDesigner(seqs, [], probe_length=100,
+            probe_stride=50, cluster_threshold=0.1, cluster_merge_after=None)
+        clustered_genomes = pb._cluster_genomes()
+
+        # g1 and g2 should be in a cluster, and g3 and g4 should be in a
+        # cluster
+        self.assertEqual(len(clustered_genomes), 2)
+        cluster1 = clustered_genomes[0]
+        cluster2 = clustered_genomes[1]
+        if g1 in cluster1:
+            self.assertEqual(set(cluster1), set([g1, g2]))
+            self.assertEqual(set(cluster2), set([g3, g4]))
+        else:
+            self.assertEqual(set(cluster1), set([g3, g4]))
+            self.assertEqual(set(cluster2), set([g1, g2]))
+
+    def test_filter_with_clusters(self):
+        """Tests running two back-to-back DuplicateFilters, with the first
+        run on each cluster and the second run on the merge of the output
+        probes from the clusters.
+        """
+        g1 = genome.Genome.from_one_seq('ATTA'*500)
+        g2 = genome.Genome.from_one_seq('TAAT'*500)
+        g3 = genome.Genome.from_one_seq('GC'*500)
+        g4 = genome.Genome.from_one_seq('CG'*500)
+        seqs = [[g1, g3], [g2, g4]]
+
+        df1 = duplicate_filter.DuplicateFilter()
+        df2 = duplicate_filter.DuplicateFilter()
+        pb = probe_designer.ProbeDesigner(seqs, [df1, df2], probe_length=100,
+            probe_stride=8, cluster_threshold=0.1, cluster_merge_after=df1)
+        pb.design()
+
+        desired_candidate_probes = ['ATTA'*25, 'TAAT'*25, 'CG'*50, 'GC'*50]
+        desired_candidate_probes = \
+            [probe.Probe.from_str(s) for s in desired_candidate_probes]
+        desired_final_probes = ['A' * 75, 'A' * 50 + 'B' * 25,
+                                'A' * 25 + 'B' * 50, 'B' * 75,
+                                'B' * 50 + 'A' * 25, 'B' * 25 + 'A' * 50]
+        desired_final_probes = ['ATTA'*25, 'TAAT'*25, 'CG'*50, 'GC'*50]
+        desired_final_probes = \
+            [probe.Probe.from_str(s) for s in desired_final_probes]
+
+        self.assertEqual(set(pb.candidate_probes), set(desired_candidate_probes))
+        self.assertCountEqual(pb.final_probes, desired_final_probes)
 
     def tearDown(self):
         # Re-enable logging
